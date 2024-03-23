d6364cc6125aab9e4d5136abae4f5f78151c2553
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2024-03-23 22:37:55 +0600

    Convert styles of  Header component into styled component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import { HeaderStyled } from "./styles/Header.style";
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </header>
+    </HeaderStyled>
   );
 }
